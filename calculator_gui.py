0ccffa019237d0fd3bd8657c92663f9fcf1240e9
anish7503 <54628032+anish7503@users.noreply.github.com>
2021-10-05 18:43:07 +0530

    update clculator_gui.py
    my version of your calculator

--- calculator_gui.py
+++ calculator_gui.py
@@ -1,140 +1,105 @@
 from tkinter import *
-import parser
-import math
 
-# Parser help us to solve mathematical operation
+root=Tk()
 
-root = Tk()
-root.title('CALCULATOR')
-root.geometry('670x450')
-root.configure(bg='Blue')
+root.geometry("400x500")
+scvalue=StringVar()
+scvalue.set("")
+screen=Entry(root,textvar=scvalue,font="Verdana 40")
+screen.pack(fill=X,padx=10,pady=10,ipadx=8)
 
-# get the user input and place it in the text field
-i = 0
-
-
-def get_variables(num):
-    global i
-    display.insert(i, num)
-    i += 1
-
-
-def calculate():
-    entire_string = display.get()
-    try:
-        a = parser.expr(entire_string).compile()
-        result = eval(a)
-        clear_all()
-        display.insert(0, result)
-
-    except EXCEPTION:
-        clear_all()
-        display.insert(0, "ERROR")
-
-
-def factorial():
-    n = int(display.get())
-    fact = math.factorial(n)
-    clear_all()
-    display.insert(0, fact)
-
-
-# adding functionality
-def get_operation(operator):
-    global i
-    length = len(operator)
-    display.insert(i, operator)
-    i += length
-
-
-# deleting entire elements on the screen
-def clear_all():
-    display.delete(0, END)
-
-
-# deleting single element on the screen
-def undo():
-    entire_string = display.get()
-    if len(entire_string):
-        new_string = entire_string[:-1]
-        clear_all()
-        display.insert(0, new_string)
+def click(event):
+    global scvalue
+    text=event.widget.cget("text")
+    if text=="C":
+        scvalue.set("")
+        screen.update()
+    elif text=="=":
+        if scvalue.get().isdigit():
+            value=int(scvalue.get())
+        else:
+            try:
+                value=eval(screen.get())
+            except Exception as e:
+                print(e)
+                value="----ERROR----"
+        scvalue.set(value)
+        screen.update()
     else:
-        clear_all()
-        display.insert(0, "ERROR")
+        scvalue.set(scvalue.get()+text)
+        screen.update()
 
+f=Frame(root)
+b=Button(f,text="C",padx=47,pady=0,fg="red",font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+b=Button(f,text="%",padx=10,pady=0,fg="red",font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+b=Button(f,text="/",padx=10,pady=0,fg="red",font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+f.pack()
 
-# adding the input field
+f=Frame(root)
+b=Button(f,text="7",padx=10,pady=0,font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+b=Button(f,text="8",padx=10,pady=0,font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+b=Button(f,text="9",padx=10,pady=0,font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+b=Button(f,text="*",padx=10,pady=0,fg="red",font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+f.pack()
 
-display = Entry(root, font=('Helvetica', '40'))
-display.grid(row=1, columnspan=6, padx=45, pady=35, sticky=W + E)
+f=Frame(root)
+b=Button(f,text="4",padx=10,pady=0,font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+b=Button(f,text="5",padx=10,pady=0,font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+b=Button(f,text="6",padx=10,pady=0,font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+b=Button(f,text="-",padx=10,pady=0,fg="red",font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+f.pack()
 
-# adding buttons to calculator
-Button(root, text="1", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(1)).grid(row=2, column=0)
-Button(root, text="2", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(2)).grid(row=2, column=1)
-Button(root, text="3", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(3)).grid(row=2, column=2)
+f=Frame(root)
+b=Button(f,text="1",padx=10,pady=0,font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+b=Button(f,text="2",padx=10,pady=0,font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+b=Button(f,text="3",padx=10,pady=0,font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+b=Button(f,text="+",padx=6,pady=0,fg="red",font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+f.pack()
 
-Button(root, text="4", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(4)).grid(row=3, column=0)
-Button(root, text="5", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(5)).grid(row=3, column=1)
-Button(root, text="6", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(6)).grid(row=3, column=2)
+f=Frame(root)
+b=Button(f,text="00",padx=0,pady=0,font="arial 25 bold")
+b.pack(side=LEFT,padx=5,pady=5)
+b.bind("<Button-1>",click)
+b=Button(f,text="0",padx=8,pady=0,font="arial 25 bold")
+b.pack(side=LEFT,padx=12,pady=5)
+b.bind("<Button-1>",click)
+b=Button(f,text=".",padx=10,pady=0,fg="red",font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+b=Button(f,text="=",padx=10,pady=0,fg="red",bg="orange",font="arial 25 bold")
+b.pack(side=LEFT,padx=10,pady=5)
+b.bind("<Button-1>",click)
+f.pack()
 
-Button(root, text="7", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(7)).grid(row=4, column=0)
-Button(root, text="8", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(8)).grid(row=4, column=1)
-Button(root, text="9", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(9)).grid(row=4, column=2)
-
-# adding other buttons to the calculator
-Button(root, text="AC", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: clear_all()).grid(row=5, column=0)
-Button(root, text="0", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_variables(0)).grid(row=5, column=1)
-Button(root, text="=", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: calculate()).grid(row=5, column=2)
-
-Button(root, text="+", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("+")).grid(row=2,
-                                                column=3)
-Button(root, text="-", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("-")).grid(row=3,
-                                                column=3)
-Button(root, text="*", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("*")).grid(row=4,
-                                                column=3)
-Button(root, text="/", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("/")).grid(row=5,
-                                                column=3)
-
-# adding new operations
-Button(root, text="pi", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("*3.14")).grid(row=2,
-                                                    column=4)
-Button(root, text="%", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("%")).grid(row=3,
-                                                column=4)
-Button(root, text="(", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("(")).grid(row=4,
-                                                column=4)
-Button(root, text="exp", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("**")).grid(row=5,
-                                                 column=4)
-
-Button(root, text=">-", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: undo()).grid(row=2, column=5)
-Button(root, text="!", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: factorial()).grid(row=3, column=5)
-Button(root, text=")", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation(")")).grid(row=4,
-                                                column=5)
-Button(root, text="^2", bg="Black", fg="White", padx=25, pady=13, font=('Helvetica', '20'),
-       command=lambda: get_operation("**2")).grid(row=5,
-                                                  column=5)
 
 root.mainloop()
